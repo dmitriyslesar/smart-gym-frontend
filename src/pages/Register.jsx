42f2763f60fc4a9db2e85adef8fc9ac2de958dce
Enchant <dmitriyslesar2005@gmail.com>
2026-06-07 23:09:37 +1000

    extending

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -36,7 +36,7 @@ const Register = () => {
     setErrors('');
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/register/', {
+      const response = await fetch(`${API_URL}/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
